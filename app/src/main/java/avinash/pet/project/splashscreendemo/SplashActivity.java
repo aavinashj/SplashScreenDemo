1135cd5b12c61876aa54b1932b0ab32446d50e94
avinash <carenxinovations@gmail.com>
2016-03-09 15:53:33 +0530

    comments

--- app/src/main/java/avinash/pet/project/splashscreendemo/SplashActivity.java
+++ app/src/main/java/avinash/pet/project/splashscreendemo/SplashActivity.java
@@ -11,6 +11,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        //you can use a timer to prolong the splash activity
         //start the next activity
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
